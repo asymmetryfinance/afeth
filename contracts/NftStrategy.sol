25eef62d0a5ea254b4cafb2a56ec3ae64813f01f
Sean Luther <elmutt@gmail.com>
2023-07-17 15:07:19 -0600

    start on mint

--- contracts/NftStrategy.sol
+++ contracts/NftStrategy.sol
@@ -5,13 +5,13 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 abstract contract NftStrategy is Initializable, OwnableUpgradeable, ERC1155Upgradeable {
     /// open new position, returns positionId
-    function mint() virtual external payable returns (uint256);
+    function mint() virtual external payable returns (uint256 positionCount);
 
     /// request to close a position
     function requestClose(uint256 positionId) virtual external;
 
     /// check if a position has fully closed and can be burned
-    function burnable(uint256 positionId) virtual external view returns (uint256);
+    function burnable(uint256 positionId) virtual external view returns (uint256 burnable);
 
     /// burn token to claim eth if burnable(positionId) is true
     function burn(uint256 positionId) virtual external;
@@ -20,11 +20,11 @@ abstract contract NftStrategy is Initializable, OwnableUpgradeable, ERC1155Upgra
     function claimRewards(uint256 positionId) virtual external;
 
     /// how much rewards can be claimed right now
-    function claimable(uint256 positionId) virtual external view returns (uint256);
+    function claimable(uint256 positionId) virtual external view returns (uint256 ethAmountClaimable);
 
     /// how much has already been claimed from a position
-    function claimed(uint256 positionId) virtual external view returns (bool);
+    function claimed(uint256 positionId) virtual external view returns (bool claimed);
 
     /// current value of a position if it were to be burned right now
-    function currentValue(uint256 positionId) virtual external view returns (uint256);
+    function currentValue(uint256 positionId) virtual external view returns (uint256 ethValue);
 }
--- contracts/strategies/votium/VotiumNft.sol
+++ contracts/strategies/votium/VotiumNft.sol
@@ -5,7 +5,17 @@ import "../../NftStrategy.sol";
 pragma solidity 0.8.19;
 
 contract VotiumStrategy is VotiumStrategyCore, NftStrategy {
-    function mint() external payable override returns (uint256 positionId) {}
+    // how many individual tokens to mint per erc1155 position
+    uint256 public positionDivisibility = 10e18;
+
+    // total positions that have ever been created
+    uint256 public positionCount;
+
+    function mint() external payable override returns (uint256) {
+        _mint(msg.sender, positionCount, positionDivisibility, "");
+        positionCount++;
+        return positionCount;
+    }
 
     function requestClose(uint256 positionId) external override {}
 
