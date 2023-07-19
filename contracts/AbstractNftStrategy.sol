1e92cf7caf5d56d2afcc05fbe1e0d68d84268e23
Sean Luther <elmutt@gmail.com>
2023-07-19 17:53:07 -0600

    clarify comments

--- contracts/AbstractNftStrategy.sol
+++ contracts/AbstractNftStrategy.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 abstract contract AbstractNftStrategy is Initializable, OwnableUpgradeable, ERC721Upgradeable {
     struct Position {
-        uint256 unlockTime; // when it can be burned. 0 if requestClose() hasn't been called
+        uint256 unlockTime; // when it can be burned (fully closed). 0 if requestClose() hasn't been called
         uint256 ethClaimed; // how much eth value has been claimed from this position so far
         uint256 ethBurned; // how much eth was received by burning tokens from this position
         uint256 startingValue; // how much eth value was locked up when the position was created
@@ -19,13 +19,13 @@ abstract contract AbstractNftStrategy is Initializable, OwnableUpgradeable, ERC7
         _;
     }
 
-    /// open new position, returns positionId
+    /// open new position (mint nft), returns positionId
     function mint() virtual external payable returns (uint256 positionId);
 
-    /// request to close a position so it can be burned later
+    /// request to close a position so nft can be burned later
     function requestClose(uint256 positionId) virtual external;
 
-    /// burn token to receive all locked value and rewards (position must be fully closed)
+    /// burn nft to receive all locked value and rewards. position must be fully closed (requestClose() called & sufficient time passed)
     function burn(uint256 positionId) virtual external;
 
     /// Withdraw any rewards from the position that can be claimed right now
