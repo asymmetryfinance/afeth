c8153d12895e33d5e947f63383bc7a4cbae6567f
Sean Luther <elmutt@gmail.com>
2023-07-19 18:42:10 -0600

    better comment

--- contracts/AfEth.sol
+++ contracts/AfEth.sol
@@ -2,5 +2,6 @@
 pragma solidity 0.8.19;
 
 contract AfEth {
-    // TODO
+    // AfEth is the strategy manager for safEth and votium strategies
+    // TODO wrap safEth and votium strategies to mint afEth nfts
 }
