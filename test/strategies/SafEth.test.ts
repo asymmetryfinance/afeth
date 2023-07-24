b37f146d6367b9d73d30cfafa5bbeff529b51821
Sean Luther <elmutt@gmail.com>
2023-07-24 12:55:10 -0600

    add test stubs

---
+++ test/strategies/SafEth.test.ts
@@ -0,0 +1,5 @@
+describe("Test SafEth Strategy", async function () {
+  it("Should test some things (TODO)", async function () {
+    // TODO
+  });
+});
---
+++ test/strategies/VotiumStrategy.test.ts
@@ -0,0 +1,5 @@
+describe("Test Votium Strategy", async function () {
+  it("Should test some things (TODO)", async function () {
+    // TODO
+  });
+});
