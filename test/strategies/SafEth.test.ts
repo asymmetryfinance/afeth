aec3bdea98d3f4caa2dcfdaf7fc5b96eb3cbeb8a
Sean Luther <elmutt@gmail.com>
2023-07-24 15:40:30 -0600

    set eth burned and add safeth test

--- test/strategies/SafEth.test.ts
+++ test/strategies/SafEth.test.ts
@@ -1,7 +1,9 @@
+import { expect } from "chai";
 import { SafEthStrategy } from "../../typechain-types";
 import { ethers, upgrades } from "hardhat";
-describe("Test SafEth Strategy Specific Functionality", async function () {
+describe.only("Test SafEth Strategy Specific Functionality", async function () {
   let safEthStrategy: SafEthStrategy;
+  let accounts: any;
   before(async () => {
     const safEthStrategyFactory = await ethers.getContractFactory(
       "SafEthStrategy"
@@ -10,10 +12,42 @@ describe("Test SafEth Strategy Specific Functionality", async function () {
       safEthStrategyFactory
     )) as SafEthStrategy;
     await safEthStrategy.deployed();
+    accounts = await ethers.getSigners();
   });
 
   it("Should mint() and be able to immediately requestClose() and burn() the position", async function () {
-    // TODO
+    const mintTx = await safEthStrategy.mint({
+      value: ethers.utils.parseEther("1"),
+    });
+    await mintTx.wait();
+
+    const positionBeforeClose = await safEthStrategy.positions(0);
+
+    expect(positionBeforeClose.unlockTime).eq(0);
+
+    await expect(safEthStrategy.burn(0)).to.be.revertedWith(
+      "requestClose() not called"
+    );
+
+    const requestCloseTx = await safEthStrategy.requestClose(0);
+    await requestCloseTx.wait();
+
+    const positionAfterClose = await safEthStrategy.positions(0);
+    const currentBlock = await ethers.provider.getBlockNumber();
+    const currentBlockTimestamp = (await ethers.provider.getBlock(currentBlock))
+      .timestamp;
+
+    expect(positionAfterClose.unlockTime).eq(currentBlockTimestamp);
+
+    const balanceBefore = await ethers.provider.getBalance(accounts[0].address);
+    const burnTx = await safEthStrategy.burn(0);
+    await burnTx.wait();
+    const balanceAfter = await ethers.provider.getBalance(accounts[0].address);
+    const positionAfterBurn = await safEthStrategy.positions(0);
+
+    expect(positionAfterClose.ethBurned).eq(0);
+    expect(positionAfterBurn.ethBurned).gt(positionAfterClose.ethBurned);
+    expect(balanceAfter).gt(balanceBefore);
   });
 
   it("Should be able to call claimRewards() but have no effect because safEth rewards are received upon burning", async function () {
