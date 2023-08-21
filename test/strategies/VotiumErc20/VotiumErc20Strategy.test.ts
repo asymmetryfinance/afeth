19b128458c2c82ba06fab1818d4a11f0608fdec1
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-08-21 09:11:35 -0600

    Split votium tests

--- test/strategies/VotiumErc20/VotiumErc20Strategy.test.ts
+++ test/strategies/VotiumErc20/VotiumErc20Strategy.test.ts
@@ -311,40 +311,4 @@ describe("Test VotiumErc20Strategy", async function () {
   it("Should never take more than 16 weeks to withdraw from the queue", async function () {
     // TODO
   });
-  it("Should always receive greater than or equal to the original cvx deposit value, even if applyRewards() is never called", async function () {
-    // TODO
-  });
-  it("Should allow a user to burn and fully withdraw from the queue without needing the owner to ever call anything", async function () {
-    // TODO
-  });
-  it("Should withdraw from the queue in order of who burned their tokens first", async function () {
-    // TODO
-  });
-  it("Should process multiple queue positions in a single call if there are enough unlockable cvx built up", async function () {
-    // TODO
-  });
-  it("Should allow anyone to process the queue", async function () {
-    // TODO
-  });
-  it("Should only allow the owner to applyRewards()", async function () {
-    // TODO
-  });
-  it("Should not be able to burn more than a users balance", async function () {
-    // TODO
-  });
-  it("Should be able to millions of dollars in rewards with minimal slippage", async function () {
-    // TODO
-  });
-  it("Should test everything about the queue to be sure it works correctly", async function () {
-    // TODO
-  });
-  it("Should allow owner to manually deposit eth rewards and price goes up", async function () {
-    // TODO
-  });
-  it("Should not change the price when minting, burning or withdrawing", async function () {
-    // TODO
-  });
-  it("Should allow owner to overide sell data and only sell some of the rewards instead of everything from the claim proof", async function () {
-    // TODO
-  });
 });
---
+++ test/strategies/VotiumErc20/VotiumErc20Strategy2.test.ts
@@ -0,0 +1,87 @@
+import { network, ethers, upgrades } from "hardhat";
+import { VotiumErc20Strategy } from "../../../typechain-types";
+import { expect } from "chai";
+import {
+  incrementVlcvxEpoch,
+  readJSONFromFile,
+  updateRewardsMerkleRoot,
+} from "./VotiumTestHelpers";
+import { BigNumber } from "ethers";
+import {
+  votiumClaimRewards,
+  votiumSellRewards,
+} from "../../../scripts/applyVotiumRewardsHelpers";
+import { within1Percent } from "../../helpers/helpers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+describe("Test VotiumErc20Strategy", async function () {
+  let votiumStrategy: VotiumErc20Strategy;
+  let accounts: SignerWithAddress[];
+
+  const resetToBlock = async (blockNumber: number) => {
+    await network.provider.request({
+      method: "hardhat_reset",
+      params: [
+        {
+          forking: {
+            jsonRpcUrl: process.env.MAINNET_URL,
+            blockNumber,
+          },
+        },
+      ],
+    });
+    accounts = await ethers.getSigners();
+    const votiumStrategyFactory = await ethers.getContractFactory(
+      "VotiumErc20Strategy"
+    );
+    votiumStrategy = (await upgrades.deployProxy(
+      votiumStrategyFactory,
+      []
+    )) as VotiumErc20Strategy;
+    await votiumStrategy.deployed();
+
+    // mint some to seed the system so totalSupply is never 0 (prevent price weirdness on withdraw)
+    const tx = await votiumStrategy.connect(accounts[11]).mint({
+      value: ethers.utils.parseEther("1"),
+    });
+    await tx.wait();
+  };
+
+  beforeEach(
+    async () => await resetToBlock(parseInt(process.env.BLOCK_NUMBER ?? "0"))
+  );
+
+  it("Should always receive greater than or equal to the original cvx deposit value, even if applyRewards() is never called", async function () {
+    // TODO
+  });
+  it("Should allow a user to burn and fully withdraw from the queue without needing the owner to ever call anything", async function () {
+    // TODO
+  });
+  it("Should withdraw from the queue in order of who burned their tokens first", async function () {
+    // TODO
+  });
+  it("Should process multiple queue positions in a single call if there are enough unlockable cvx built up", async function () {
+    // TODO
+  });
+  it("Should only allow the owner to applyRewards()", async function () {
+    // TODO
+  });
+  it("Should not be able to burn more than a users balance", async function () {
+    // TODO
+  });
+  it("Should be able to millions of dollars in rewards with minimal slippage", async function () {
+    // TODO
+  });
+  it("Should test everything about the queue to be sure it works correctly", async function () {
+    // TODO
+  });
+  it("Should allow owner to manually deposit eth rewards and price goes up", async function () {
+    // TODO
+  });
+  it("Should not change the price when minting, burning or withdrawing", async function () {
+    // TODO
+  });
+  it("Should allow owner to overide sell data and only sell some of the rewards instead of everything from the claim proof", async function () {
+    // TODO
+  });
+});
