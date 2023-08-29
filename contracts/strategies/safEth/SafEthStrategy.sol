a94db90f34b07a1fbe6eafe648bce3313b3d06d4
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-08-29 08:21:15 -0600

    Add safeth functions

--- contracts/strategies/safEth/SafEthStrategy.sol
+++ contracts/strategies/safEth/SafEthStrategy.sol
@@ -7,14 +7,31 @@ import "../../external_interfaces/ISafEth.sol";
 
 contract SafEthStrategy is AbstractErc20Strategy, SafEthStrategyCore {
     function deposit() external payable virtual override {
-        revert("not implemented");
+        uint256 mintAmount = ISafEth(safEthAddress).stake{value: msg.value}(
+            0 // TODO: set minAmount
+        );
+        _mint(msg.sender, mintAmount);
     }
 
     function requestWithdraw(uint256 _amount) external virtual override {
-        revert("not implemented");
+        /// noop
     }
 
-    function withdraw(uint256 epochToWithdraw) external virtual override {
-        revert("not implemented");
+    function withdraw(uint256) external virtual override {
+        uint256 amount = balanceOf(msg.sender);
+        _burn(msg.sender, amount);
+
+        uint256 ethBalanceBefore = address(this).balance;
+
+        ISafEth(safEthAddress).unstake(
+            amount,
+            0 // TODO: set minAmount
+        );
+        uint256 ethBalanceAfter = address(this).balance;
+        uint256 ethReceived = ethBalanceAfter - ethBalanceBefore;
+
+        // solhint-disable-next-line
+        (bool sent, ) = msg.sender.call{value: ethReceived}("");
+        require(sent, "Failed to send Ether");
     }
 }
