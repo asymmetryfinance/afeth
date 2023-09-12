5eb052575b1d224d0ca3d2db9f0505f87a9f7f1d
Sean Luther <elmutt@gmail.com>
2023-09-12 17:30:18 -0600

    good

--- contracts/strategies/votiumErc20/VotiumErc20StrategyCore.sol
+++ contracts/strategies/votiumErc20/VotiumErc20StrategyCore.sol
@@ -198,7 +198,7 @@ contract VotiumErc20StrategyCore is
     }
 
     /**
-     * @notice - Allows owner to withdraw any stuck erc20 tokens 
+     * @notice - Allows owner to withdraw any stuck erc20 tokens
      * @notice - lets us handle any that were not successfully sold via cvx
      * @param _token - address of the token to withdraw
      */
