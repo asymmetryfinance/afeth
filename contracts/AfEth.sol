ccd3318a92e7d94bbb4edaecbce443220c9dfa2c
toshiSat <10103480+toshiSat@users.noreply.github.com>
2023-09-16 09:09:54 -0600

    Remove merge messup

--- contracts/AfEth.sol
+++ contracts/AfEth.sol
@@ -312,19 +312,16 @@ contract AfEth is Initializable, OwnableUpgradeable, ERC20Upgradeable {
             if (!sent) revert FailedToSend();
         }
         uint256 amount = _amount - feeAmount;
-        if (safEthAddress != address(0)) {
-            uint256 safEthTvl = (ISafEth(
-                0x6732Efaf6f39926346BeF8b821a04B6361C4F3e5
-            ).approxPrice(false) * IERC20(safEthAddress).totalSupply()) / 1e18;
-            uint256 votiumTvl = ((votiumStrategy.cvxPerVotium() *
-                votiumStrategy.ethPerCvx()) *
-                IERC20(vEthAddress).totalSupply()) / 1e36;
-            uint256 totalTvl = (safEthTvl + votiumTvl);
-            uint256 safEthRatio = (safEthTvl * 1e18) / totalTvl;
-            if (safEthRatio < ratio)
-                this.applyStrategyReward{value: amount}(safEthAddress);
-            else votiumStrategy.depositRewards{value: amount}(amount);
-        }
+        uint256 safEthTvl = (ISafEth(0x6732Efaf6f39926346BeF8b821a04B6361C4F3e5)
+            .approxPrice(false) * IERC20(safEthAddress).totalSupply()) / 1e18;
+        uint256 votiumTvl = ((votiumStrategy.cvxPerVotium() *
+            votiumStrategy.ethPerCvx()) * IERC20(vEthAddress).totalSupply()) /
+            1e36;
+        uint256 totalTvl = (safEthTvl + votiumTvl);
+        uint256 safEthRatio = (safEthTvl * 1e18) / totalTvl;
+        if (safEthRatio < ratio)
+            this.applyStrategyReward{value: amount}(safEthAddress);
+        else votiumStrategy.depositRewards{value: amount}(amount);
     }
 
     receive() external payable {}
