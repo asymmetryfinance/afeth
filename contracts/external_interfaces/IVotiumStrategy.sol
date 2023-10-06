b06dbf3551b7142dd33deae694fe02aa40e147ce
Sean Luther <elmutt@gmail.com>
2023-10-05 18:37:27 -0600

    undo change

--- contracts/external_interfaces/IVotiumStrategy.sol
+++ contracts/external_interfaces/IVotiumStrategy.sol
@@ -6,5 +6,5 @@ interface IVotiumStrategy {
 
     function ethPerCvx(bool validate) external view returns (uint256);
 
-    function depositRewards(uint256 amount, uint256 _cvxMinout) external payable;
+    function depositRewards(uint256 amount) external payable;
 }
